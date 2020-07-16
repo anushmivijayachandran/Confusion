341d3bc44582cb5a95e76d1bd3bb974cbd121342
anushmivijayachandran <anushmivijayachandra@cmail.carleton.ca>
2020-07-16 15:32:44 -0300

    Fetch handling errors

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -39,7 +39,7 @@ const Menu = (props) => {
     return(
       <div className="container">
         <div className="row">
-          <h4>props.dishes.errMess</h4>
+          <h4>{props.dishes.errMess}</h4>
         </div>
       </div>
     );
